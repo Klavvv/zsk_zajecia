4b567de8399cdb788f2a323d8be9792e1d79a536
Klaudiusz Rozenek <klaudiusz.rozenek@uczen.zsk.poznan.pl>
2020-09-16 09:31:09 +0200

    lekcja 2

---
+++ PAI/operacje_na_zmiennych.php
@@ -0,0 +1,25 @@
+<?php
+  // wersja php
+  echo PHP_VERSION,'<br>';
+
+  // echo phpinfo()
+  // interpolacja
+  echo 'a','b','c<br>'; // show a show b show c
+  echo 'a'.'b'.'c<br>'; // a+b+c show abc
+
+  $pow=2**10;
+  echo "$pow<br>";
+
+  //operatory bitowe
+  // and &, or |, not ~, xor ^, <<, >>
+  $bin=0b1010;
+  echo "<br>$bin<br>";
+
+  $bin=$bin<<2; // 101000
+  echo $bin; //40
+
+  $bin=$bin >> 3; // 101
+  echo $bin; // 5
+
+
+?>
--- PAI/zmienne.php
+++ PAI/zmienne.php
@@ -1,14 +1,53 @@
 <?php
+// podstawa
   $name="Janusz";
   $surname="Nowak";
   echo "Imię:$name<br>";
   echo 'Imię:$name<br>';
   // konkatenacja .
   echo "Nazwisko:$surname"."<br>";
-  echo "Metheus";
 
-  // typy danych
+// typy danych
   // boolean
-  
+  $prawda=true;
+  $fałsz=false;
+  echo $prawda; //1
+  echo "$fałsz<br>"; //nic nie wyświetli
+
+  // intiger
+  $bin=0b1011; //11
+  $oct=011; //9
+  $dec=11; //11
+  $hex=0xA; //10
+  echo "$hex<hr>";
+
+  // składnia heredoc
+  $name='Janusz';
+  $surname='Nowak';
+
+  $text=<<<LABEL
+  Imię: $name<br>
+  Nazwisko: $surname<br>
+  <hr>
+  LABEL;
+  echo $text;
+
+  echo <<<L
+  heredoc 2<br>
+  Imię: $name<hr>
+  L;
+
+  // nowdoc
+  echo <<<'L'
+  nowdoc<br>
+  Imię: $name<hr>
+  L;
+
+  $city="Poznań";
+  echo "Nazwa zmiennej: \$city<br>";
+  echo "Nazwa zmiennej: $city";
+
+
+
 
 ?>
