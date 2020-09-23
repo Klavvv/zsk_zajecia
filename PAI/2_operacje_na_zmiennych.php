1d88bbc863786da031284d652e6cda54afbc2447
Klaudiusz Rozenek <klaudiusz.rozenek@uczen.zsk.poznan.pl>
2020-09-23 09:17:28 +0200

    asdasd

--- PAI/2_operacje_na_zmiennych.php
+++ PAI/2_operacje_na_zmiennych.php
@@ -19,7 +19,45 @@
   echo $bin; //40
 
   $bin=$bin >> 3; // 101
-  echo $bin; // 5
+  echo "$bin<br>"; // 5
+
+// porównanie
+  $x=1;
+  $y=1.0;
+  if($x===$y){
+    echo '$x jest równa $y';
+  }
+  else{
+    echo '$x nie jest równa $y<br>';
+  }
+
+  echo "<br>";
+  echo gettype($x); //intiger
+  echo "<br>";
+  echo gettype($y); //double
+  echo "<br>";
+
+
+  $x=10;
+  $y=100;
+  echo $x <=> $y;
+  echo '<hr>';
+
+  $x=1;
+  $x=$x++;
+  echo $x;  //1
+  $x=++$x;
+  echo $x;  //2
+  $y=$x++;
+  echo $x;  //3
+  echo $y;  //2
+  $y=++$x*2-1;
+  echo $x;  //4
+  echo $y;  //7
+
+// operatory rzutowania
+
+
 
 
 ?>
